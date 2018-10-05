4f34d895610e33b7fe9c37a79b6660777a5c8fbc
gettinghigh <eogkdltys@gmail.com>
2018-10-05 16:20:51 +0800

    ..

--- jquery.js
+++ jquery.js
@@ -8,12 +8,23 @@ $(".up_rec").animate({left:'0px'},);
 $(".down_rec").animate({left:'0px'},"slow");
 $(".up_rec").delay(700).animate({left:'66%'},)
 $(".down_rec").delay(700).animate({left:'66%'},"slow")
+
+$(".button").click(function(){
+    //alert("The button was clicked.");
+    var position = $(".bigbox").offset();  
+	var x = position.left;  
+	var y = position.top; 
+	
+	var w = $(".up_rec").width();
+	var h = $(".up_rec").height();
+	
+	$("#white").height(h*2);
+	$("#white").width(w*0.66);
+	$("#white").css('left', x);
+	$("#white").css('top', y);
+	$("#white").css('visibility', "visible");
 });
-
-
-	  $(".up_rec").animate({left:'66%'},)
-	  $(".down_rec").animate({left:'66%'},"slow")
-
+});
 
 /*
 $(document).ready(function(){
